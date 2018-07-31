a02f75b27bc9c970291b8923c36acda0be09914d
shadowquark <shadowquark@outlook.com>
2018-07-31 02:38:31 +0200

    20180730

--- tools/analysis/src/analyses/CMS_13TeV/cms_1806_05246.cc
+++ tools/analysis/src/analyses/CMS_13TeV/cms_1806_05246.cc
@@ -58,6 +58,123 @@ void Cms_1806_05246::analyze() {
 
   missingET->addMuons(muonsCombined);  // Adds muons to missing ET. This should almost always be done which is why this line is not commented out.
   
+// Coded by yyFish ############################################################
+	muonsCombined = filterIsolation( muonsCombined , 0 );
+	electronsTight = filterIsolation( electronsLoose , 0 );
+	muonsCombined = filterPhaseSpace( muonsCombined , 10 , -2.4 , 2.4 );
+	electronsTight = filterPhaseSpace( electronsLoose , 10 , -2.5 , 2.5 );
+	bool flag[2] = {};
+	if ( muonsCombined.size() + electronsTight.size() < 3 )	return;
+	if ( muonsCombined.size() + electronsTight.size() > 3 )	
+		flag[0] = 1;
+	if ( muonsCombined.size() + electronsTight.size() > 4 )	
+		flag[1] = 1;
+	if ( !flag[0] && muonsCombined.size() == 2 &&\
+		muonsCombined[0]->Charge + muonsCombined[1]->Charge == 0 )
+	{
+		vector<double> lRank = { muonsCombined[0]->PT , muonsCombined[1]->PT ,\
+					electronsTight[0]->PT };
+		sort( lRank.begin() , lRank.end() ,\
+			[]( double x , double y ){ return x > y; } );
+		if ( lRank[0] <= 25 || lRank[1] <= 20 || lRank[2] <= 15 )	return;
+		double tmpMll = ( muonsCombined[0]->P4() + muonsCombined[1]->P4() ).M();
+		if ( muonsCombined[0]->Charge + electronsTight[0]->Charge == 0 &&\
+			( muonsCombined[0]->P4() + electronsTight[0]->P4() ).M() < tmpMll )
+			tmpMll = ( muonsCombined[0]->P4() + electronsTight[0]->P4() ).M();
+		if ( muonsCombined[1]->Charge + electronsTight[0]->Charge == 0 &&\
+			( muonsCombined[1]->P4() + electronsTight[0]->P4() ).M() < tmpMll )
+			tmpMll = ( muonsCombined[1]->P4() + electronsTight[0]->P4() ).M();
+		if ( tmpMll <= 12 )	return;
+		if ( tmpMll >= 100 )	return;
+		jets = filterPhaseSpace( jets , 30 , -4.7 , 4.7 );
+		if ( jets.size() )	return;
+		if ( missingET->PT <= 50 )	return;
+		TLorentzVector dilep = muonsCombined[0]->P4() + muonsCombined[1]->P4();
+		if ( abs( dilep.M() - 91.1876 ) <= 25 )	return;
+		dilep += electronsTight[0]->P4();
+		if ( abs( missingET->P4().DeltaPhi(dilep) ) <= 2.2 )	return;
+		countSignalEvent( "3l" );
+	}
+	if ( !flag[1] && muonsCombined.size() == 4 )
+	{
+		int totCharge = 0;
+		for ( auto i : muonsCombined )
+			totCharge += i->Charge;
+		if (totCharge)	return;
+		if ( muonsCombined[0]->PT <= 25 || muonsCombined[1]->PT <= 15 )
+			return;	
+		TLorentzVector diZ , diX , l4 = muonsCombined[1]->P4() +\
+						muonsCombined[2]->P4() + muonsCombined[3]->P4();
+		for ( auto i : muonsCombined )
+			if ( muonsCombined[0]->Charge + i->Charge == 0 )
+			{
+				diZ = muonsCombined[0]->P4() + i->P4();
+				diX = l4 - i->P4();
+				if ( diZ.M() <= 4 || diX.M() <= 4 )	return;
+				break;
+			}
+		if ( abs( diZ.M() - 91.1876 ) > abs( diX.M() - 91.1876 ) )
+		{
+			TLorentzVector temp = diZ;
+			diZ = diX;
+			diX = temp;
+		}
+		if ( abs( diZ.M() - 91.1876 ) >= 15 )	return;
+		if ( diX.M() <= 10 || diX.M() >= 50 )	return;
+		if ( missingET->PT <= 35 || missingET->PT >= 100 )	return;
+		if ( ( diX + diZ ).M() <= 140 )	return;
+		countSignalEvent( "4l" );
+	}
+	if ( !flag[1] && muonsCombined.size() == 3 && electronsTight.size() == 1 )
+	{
+		int totCharge = electronsTight[0]->Charge;
+		vector<double> lRank = { electronsTight[0]->PT };
+		TLorentzVector diZ , diX , l4 = electronsTight[0]->P4();
+		for ( auto i : muonsCombined )
+		{
+			l4 += i->P4();
+			totCharge += i->Charge;
+			lRank.push_back( i->PT );
+			if ( muonsCombined[0]->Charge + i->Charge == 0 )
+				diZ = muonsCombined[0]->P4() + i->P4();
+		}
+		diX = l4 - diZ;
+		if (totCharge)	return;
+		sort( lRank.begin() , lRank.end() ,\
+			[]( double x , double y ){ return x > y; } );
+		if ( lRank[0] <= 25 || lRank[1] <= 15 )	return;	
+		if ( abs( diZ.M() - 91.1876 ) >= 15 )	return;
+		if ( diX.M() <= 10 || diX.M() >= 50 )	return;
+		if ( missingET->PT <= 35 || missingET->PT >= 100 )	return;
+		if ( ( diX + diZ ).M() <= 140 )	return;
+		if ( diX.M() <= 4 || diZ.M() <= 4 )	return;
+		countSignalEvent( "4l" );
+	}
+	if ( !flag[1] && muonsCombined.size() == 2 && electronsTight.size() == 2 )
+	{
+		if ( muonsCombined[0]->Charge + muonsCombined[1]->Charge )	return;
+		if ( electronsTight[0]->Charge + electronsTight[1]->Charge )	return;
+		TLorentzVector diZ = muonsCombined[0]->P4() + muonsCombined[1]->P4();
+		TLorentzVector diX = electronsTight[0]->P4() + electronsTight[1]->P4();
+		if ( abs( diZ.M() - 91.1876 ) > abs( diX.M() - 91.1876 ) )
+		{
+			TLorentzVector temp = diZ;
+			diZ = diX;
+			diX = temp;
+		}
+		vector<double> lRank = { electronsTight[0]->PT , electronsTight[1]->PT ,\
+					muonsCombined[0]->PT , muonsCombined[1]->PT };
+		sort( lRank.begin() , lRank.end() ,\
+			[]( double x , double y ){ return x > y; } );
+		if ( lRank[0] <= 25 || lRank[1] <= 15 )	return;	
+		if ( abs( diZ.M() - 91.1876 ) >= 15 )	return;
+		if ( diX.M() <= 10 || diX.M() >= 50 )	return;
+		if ( missingET->PT <= 35 || missingET->PT >= 100 )	return;
+		if ( ( diX + diZ ).M() <= 140 )	return;
+		if ( diX.M() <= 4 || diZ.M() <= 4 )	return;
+		countSignalEvent( "4l" );
+	}
+// NOT Double Checked #########################################################
 }
 
 void Cms_1806_05246::finalize() {
