d5645198180df22b2985b5e37e56d776d6ad143c
shadowquark <shadowquark@outlook.com>
2018-08-13 02:45:09 +0200

    20180812

--- tools/analysis/src/analyses/ATLAS_13TeV/atlas_1808_01899.cc
+++ tools/analysis/src/analyses/ATLAS_13TeV/atlas_1808_01899.cc
@@ -58,6 +58,75 @@ void Atlas_1808_01899::analyze() {
 
   missingET->addMuons(muonsCombined);  // Adds muons to missing ET. This should almost always be done which is why this line is not commented out.
   
+// Coded by yyFish #############################################################
+	if ( missingET->PT <= 60 )	return;
+	electronsLoose = filterPhaseSpace( electronsLoose , 10 , -2.47 , 2.47 , 1 );
+	muonsCombined = filterPhaseSpace( muonsCombined , 10 , -2.5 , 2.5 );
+	jets = filterPhaseSpace( jets , 25 , -2.5 , 2.5 );
+	jets = overlapRemoval( jets , electronsLoose , 0.2 );
+	jets = overlapRemoval( jets , muonsCombined , 0.2 );
+	if ( jets.size() )	return;
+	if ( electronsLoose.size() + muonsCombined.size() - 4 )	return;
+	if ( electronsLoose.size() == 2 &&\
+		electronsLoose[0]->Charge + electronsLoose[1]->Charge )
+		return;
+	if ( muonsCombined.size() == 2 &&\
+		muonsCombined[0]->Charge + muonsCombined[1]->Charge )
+		return;
+	if ( electronsLoose.size() == 1 || muonsCombined.size() == 1 )	return;
+	int totCharge = 0;
+	struct L4
+	{
+		int charge , flav;
+		double pT;
+		TLorentzVector p4;
+	};
+	vector<L4> l4;
+	for ( auto i : electronsLoose )
+	{
+		totCharge += i->Charge;
+		l4.push_back( L4{ i->Charge , 0 , i->PT , i->P4() } );
+	}
+	for ( auto i : muonsCombined )
+	{
+		totCharge += i->Charge;
+		l4.push_back( L4{ i->Charge , 1 , i->PT , i->P4() } );
+	}
+	if (totCharge)	return;
+	sort( l4.begin() , l4.end() ,\
+		[]( L4 x , L4 y ){ return x.pT > y.pT; } );
+	if ( l4[0].pT <= 30 )	return;
+	TLorentzVector mXl;
+	double dRmin = 10 , dRmax = 0;
+	for ( auto i : l4 )
+	{
+		mXl += i.p4;
+		for ( auto j : l4 )
+		{
+			if ( i.charge - j.charge && i.flav == j.flav )
+			{
+				if ( ( i.p4 + j.p4 ).M() <= 15)	return;
+				if ( abs( ( i.p4 + j.p4 ).M() - 91.1876 ) <= 10 )	return;
+			}
+			if ( i.charge + j.charge && i.p4.DeltaR( j.p4 ) < dRmin )
+				dRmin = i.p4.DeltaR( j.p4 );
+			if ( i.charge + j.charge && i.p4.DeltaR( j.p4 ) > dRmax )
+				dRmax = i.p4.DeltaR( j.p4 );
+		}
+	}
+	if ( mXl.M() > 230 && l4[0].pT > 65 &&\
+		0.16 <= dRmin && dRmin <= 1.21 && 0.27 <= dRmax && dRmax <= 2.03 )
+		countSignalEvent( "SR1" );
+	if ( mXl.M() > 270 && l4[0].pT > 45 &&\
+		0.09 <= dRmin && dRmin <= 1.97 && 0.44 <= dRmax && dRmax <= 2.68 )
+		countSignalEvent( "SR2" );
+	if ( mXl.M() > 270 && l4[0].pT > 110 &&\
+		0.39 <= dRmin && dRmin <= 2.22 && 0.55 <= dRmax && dRmax <= 2.90 )
+		countSignalEvent( "SR3" );
+	if ( mXl.M() > 370 && l4[0].pT > 160 &&\
+		0.53 <= dRmin && dRmin <= 3.24 && 0.59 <= dRmax && dRmax <= 2.94 )
+		countSignalEvent( "SR4" );
+// NOT Double Checked ##########################################################
 }
 
 void Atlas_1808_01899::finalize() {
